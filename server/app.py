a27287f73a12e83a7d6f80d26f89f34035a0c723
Jakub Ciszek <jakubciszek64@gmail.com>
2019-03-22 17:46:54 +0100

    REST api support

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask("It's alive!")
+api = Api(app)
+
+
+class ApiServer(Resource):
+    def get(self):
+        return {'halo?': 'halo!'}
+
+
+api.add_resource(ApiServer, '/api/')
 
 
 @app.route('/')
