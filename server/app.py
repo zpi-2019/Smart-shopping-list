49ef79097ebc1590465712dda715edb4a21be8df
Jakub Ciszek <jakubciszek64@gmail.com>
2019-05-16 14:45:45 +0200

    MongoDB Atlas connection

--- server/app.py
+++ server/app.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify, request, redirect, url_for
 from flask_restful import Resource, Api, reqparse
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+from os import environ
 
 app = Flask("It's alive!")
 api = Api(app)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ShoppingListDb"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/ShoppingListDb"
+app.config["MONGO_URI"] = environ['MONGODB_CONNECTION_URL']
 mongo = PyMongo(app)
 
 parser = reqparse.RequestParser()
@@ -15,20 +17,21 @@ parser.add_argument('userID', type=int)
 
 class ApiServer(Resource):
     def get(self):
-        return {'halo?': 'halo!'}
+        data = mongo.db.shopping_lists.find_one({"_id": 1})
+        return data
 
 
 class ShoppingList(Resource):
     def get(self, userID=None):
         if userID:
-            shopping_lists = mongo.db.List.find({'userID' : userID})
+            shopping_lists = mongo.db.List.find({'userID': userID})
         else:
             shopping_lists = mongo.db.List.find()
         return dumps(shopping_lists)
 
     def put(self, userID):  # TODO replace userID with unique attribute (possibly ObjectID)
         args = parser.parse_args()
-        mongo.db.List.update_one({'userID' : userID}, {'$set' : {'products' : args['products']}})
+        mongo.db.List.update_one({'userID': userID}, {'$set': {'products': args['products']}})
         return redirect(url_for('userID', userID=userID))
 
     def post(self):
